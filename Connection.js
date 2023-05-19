58e806920f9f138944068bc19f9d8ab6f09c8f59
Getinet Alemu Mijena <121330349+Getinet-Alemu-Mijena@users.noreply.github.com>
2023-05-19 21:51:57 +0300

    first commit

---
+++ Connection.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+const port = 3050;
+const cors = require("cors");
+
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "payment",
+});
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to MySQL server");
+  }
+});
